68e7f9582ae54ba5b397b4eefc361b44d6ff49e0
Ryan Bemrose <pi@bemrose.us>
2018-04-22 13:23:05 -0700

    0.5.5 Change spawn-on-cube tweak to only apply to blocks that have a collision box.  It was blocking all water mob spawning

--- src/main/java/us/bemrose/mc/pitweaks/SpawnOnCubeTweak.java
+++ src/main/java/us/bemrose/mc/pitweaks/SpawnOnCubeTweak.java
@@ -1,6 +1,8 @@
 package us.bemrose.mc.pitweaks;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.Block;
 
 public class SpawnOnCubeTweak extends Tweak {
 
@@ -9,17 +11,18 @@ public class SpawnOnCubeTweak extends Tweak {
 
     @Override
     public void init(net.minecraftforge.fml.common.event.FMLInitializationEvent event) {
-        // Register block place event
         net.minecraftforge.common.MinecraftForge.EVENT_BUS.register(this);
     }
     
     @net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-	public void onSpawnCheck(net.minecraftforge.event.entity.living.LivingSpawnEvent.CheckSpawn event) {
+    public void onSpawnCheck(net.minecraftforge.event.entity.living.LivingSpawnEvent.CheckSpawn event) {
 
-		BlockPos pos = event.getEntity().getPosition();
-        
-		if(!event.getWorld().getBlockState(pos.down()).isFullCube()) {
-			event.setResult(net.minecraftforge.fml.common.eventhandler.Event.Result.DENY);
-        }    
+        BlockPos pos = event.getEntity().getPosition().down();
+        IBlockState state = event.getWorld().getBlockState(pos);
+
+        // Cancel the spawn if the block below has a collision box, but is not a full cube
+        if(!(state.isFullCube() || state.getCollisionBoundingBox(event.getWorld(), pos) == Block.NULL_AABB)) {
+            event.setResult(net.minecraftforge.fml.common.eventhandler.Event.Result.DENY);
+        }
     }
 }
